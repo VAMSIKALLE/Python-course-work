bbc948c53a87996055d6d7f40581f7240227ff61
Kalle Vamsi <vamsikalle360@gmail.com>
2026-05-27 17:47:55 +0530

    Add files via upload

---
+++ Day2/variable.py
@@ -0,0 +1,16 @@
+Python 3.14.5 (tags/v3.14.5:5607950, May 10 2026, 10:43:50) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+============================================= RESTART: C:/Users/HP/OneDrive/Desktop/PYTHON-COURSE-WORK/Day2/keyword.py ============================================
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+35
+>>> Myvar=10
+>>> MYvar1=10
+>>> Myvar@1=10
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+>>> my_var=10
+>>> my var=10]
+SyntaxError: unmatched ']'
+>>> myvar=10
+>>> my var=10
+SyntaxError: invalid syntax
